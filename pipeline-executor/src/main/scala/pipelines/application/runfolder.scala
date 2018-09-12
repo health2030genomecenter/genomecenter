212a7d192225da609b64cd7e61001cef353e3ed2
Istvan Bartha <bartha.pityu@gmail.com>
2018-09-12 10:57:35 +0200

    Test FolderWatcherEventSource

--- pipeline-executor/src/main/scala/pipelines/application/runfolder.scala
+++ pipeline-executor/src/main/scala/pipelines/application/runfolder.scala
@@ -9,6 +9,7 @@ import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import java.nio.file.FileSystems
 import java.io.File
 import fileutils.openSource
+import com.typesafe.scalalogging.StrictLogging
 
 case class RunfolderReadyForProcessing(runId: String,
                                        sampleSheet: SampleSheet,
@@ -31,10 +32,17 @@ case class CompositeSequencingCompleteEventSource(
       Merge(count))
 }
 
-case class FolderWatcherEventSource(path: String,
-                                    fileName: String,
+/** Listens on file creation events from the filesystem
+  *
+  * Writing data to a file and creating the file are not atomic operations,
+  * thus this method is prone to race condition unless the file is linked (moved) atomically to the
+  * destination path.
+  */
+case class FolderWatcherEventSource(folderWhereRunFoldersArePlaced: String,
+                                    fileSignalingCompletion: String,
                                     sampleSheetFileName: String)
-    extends SequencingCompleteEventSource {
+    extends SequencingCompleteEventSource
+    with StrictLogging {
   private val fs = FileSystems.getDefault
 
   private def readFolder(runFolder: File): RunfolderReadyForProcessing = {
@@ -45,13 +53,26 @@ case class FolderWatcherEventSource(path: String,
   }
 
   def events: Source[RunfolderReadyForProcessing, _] =
-    DirectoryChangesSource(fs.getPath(path),
+    DirectoryChangesSource(fs.getPath(folderWhereRunFoldersArePlaced),
                            pollInterval = 1 second,
-                           maxBufferSize = 1000).collect {
-      case (path, DirectoryChange.Creation)
-          if path.getFileName.toString == fileName =>
-        readFolder(path.toFile)
-    }
+                           maxBufferSize = 1000)
+      .flatMapConcat {
+        case (potentialRunFolder, DirectoryChange.Creation) =>
+          DirectoryChangesSource(potentialRunFolder,
+                                 pollInterval = 1 second,
+                                 maxBufferSize = 1000)
+        case _ => Source.empty
+      }
+      .map { event =>
+        logger.debug(s"Directory change event $event")
+        event
+      }
+      .collect {
+        case (fileInRunFolder, DirectoryChange.Creation)
+            if fileInRunFolder.getFileName.toString == fileSignalingCompletion =>
+          val runFolder = fileInRunFolder.toFile.getParentFile
+          readFolder(runFolder)
+      }
 
 }
 
---
+++ pipeline-executor/src/test/scala/pipelines/FolderWatcherEventSourceTest.scala
@@ -0,0 +1,76 @@
+package org.gc.pipelines
+
+import org.scalatest._
+import org.scalatest.{Matchers, BeforeAndAfterAll}
+
+import scala.concurrent.duration._
+import java.io.File
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Sink
+import akka.stream.ActorMaterializer
+import akka.testkit.{TestKit, TestProbe}
+
+import org.gc.pipelines.application._
+import fileutils._
+
+class FolderWatcherEventSourceTest
+    extends TestKit(ActorSystem("FolderWatcherEventSourceTest"))
+    with FunSuiteLike
+    with BeforeAndAfterAll
+    with GivenWhenThen
+    with Matchers {
+
+  override def afterAll: Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  test(
+    "FolderWatcherEventSource should emit an event when the respective file is created") {
+    implicit val materializer = ActorMaterializer()
+    val watchedFolder = {
+      val tmpFile = TempFile.createTempFile("test")
+      tmpFile.delete
+      tmpFile.mkdir
+      tmpFile
+    }
+
+    val fileNameToWatch = "something"
+    val sampleSheetFileName = "samplesheetfilename"
+    val sampleSheetFileContent = "blabla"
+    val runId = "runid"
+    val runFolder = new File(watchedFolder, runId)
+
+    Given("a folder watch event source")
+    val source =
+      FolderWatcherEventSource(folderWhereRunFoldersArePlaced =
+                                 watchedFolder.getAbsolutePath,
+                               fileSignalingCompletion = fileNameToWatch,
+                               sampleSheetFileName = sampleSheetFileName).events
+
+    val probe = TestProbe()
+    source.to(Sink.actorRef(probe.ref, "completed")).run()
+    When("a run folder is created")
+    runFolder.mkdir
+    And("a sample sheet is created")
+    openFileWriter(new File(runFolder, sampleSheetFileName)) { writer =>
+      writer.write(sampleSheetFileContent)
+    }
+    Then("the source should not emit")
+    probe.expectNoMessage(3 seconds)
+    When("the watched file is created")
+    val watchedFile = new File(runFolder, fileNameToWatch)
+    openFileWriter(watchedFile)(_ => ())
+    Then("the source should emit and the sample sheet should be read")
+    probe.expectMsg(
+      RunfolderReadyForProcessing(runId,
+                                  SampleSheet(sampleSheetFileContent),
+                                  runFolder.getAbsolutePath))
+    When("the watched file is deleted")
+    watchedFile.delete
+    Then("the source should not emit")
+    probe.expectNoMessage(3 seconds)
+
+  }
+
+}
