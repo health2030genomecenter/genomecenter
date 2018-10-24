8b85d47486749574f269d155ba43842953cb1c08
Istvan Bartha <bartha.pityu@gmail.com>
2018-10-24 11:12:41 +0200

    Do not copy test data to container in test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,14 @@ IMAGENAME=docker-centos7-slurm-sbt:18.08.8-1.2.1
 
 cd docker/slurm-sbt-base && docker build -t $IMAGENAME .  && cd ../..
 
-NAME=`docker run --detach -v $HOME/.ivy2:/root/.ivy2  -h ernie $IMAGENAME tail -f /dev/null`
+GC_ABS_TESTFOLDER=`pwd`/$GC_TESTFOLDER
 
-docker cp . $NAME:/opt/
+NAME=`docker run --detach -v $HOME/.ivy2:/root/.ivy2  -v $GC_ABS_TESTFOLDER:/opt/testdata:ro  -h ernie $IMAGENAME tail -f /dev/null`
 
-docker cp $GC_TESTFOLDER $NAME:/opt/testdata
+docker cp -a . $NAME:/opt/
 
-docker exec -it $NAME /bin/bash -c "cd /opt && GC_TESTFOLDER=/opt/testdata/ sbt 'test' ; bash"
+# docker cp -a $GC_TESTFOLDER $NAME:/opt/testdata
+
+docker exec -it $NAME /bin/bash -c "cd /opt && GC_TESTFOLDER=/opt/testdata/ sbt -J-Dsbt.io.jdktimestamps=true 'testOnly *DemultiplexingTestSuite' ; bash"
 
 docker rm -f -v $NAME
