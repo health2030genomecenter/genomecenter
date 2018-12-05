c650275de0516ca3a09b453f0db733e1db7bcd98
Istvan Bartha <bartha.pityu@gmail.com>
2018-12-05 13:04:38 +0100

    Update StableSet test

--- pipeline-executor/src/test/scala/pipelines/util/StableSetTest.scala
+++ pipeline-executor/src/test/scala/pipelines/util/StableSetTest.scala
@@ -2,16 +2,18 @@ package org.gc.pipelines.util
 
 import org.scalatest._
 
+import io.circe.syntax._
+
 class StableSetTestSuite extends FunSuite with Matchers {
   test("maintain stable sort order irrespective of constructor") {
-    StableSet(1).toSeq shouldBe Seq(1)
-    StableSet(2, 1).toSeq shouldBe Seq(1, 2)
-    StableSet(1, 2).toSeq shouldBe Seq(1, 2)
-    StableSet(1, 2, 3).toSeq shouldBe Seq(1, 2, 3)
-    StableSet(3, 2, 1).toSeq shouldBe Seq(1, 2, 3)
-    StableSet(1, 3, 2).toSeq shouldBe Seq(1, 2, 3)
-    StableSet(2, 3, 1).toSeq shouldBe Seq(1, 2, 3)
-    StableSet(2, 1, 3).toSeq shouldBe Seq(1, 2, 3)
+    StableSet(1).asJson.noSpaces shouldBe "[1]"
+    StableSet(2, 1).asJson.noSpaces shouldBe "[1,2]"
+    StableSet(1, 2).asJson.noSpaces shouldBe "[1,2]"
+    StableSet(1, 2, 3).asJson.noSpaces shouldBe "[1,2,3]"
+    StableSet(3, 2, 1).asJson.noSpaces shouldBe "[1,2,3]"
+    StableSet(1, 3, 2).asJson.noSpaces shouldBe "[1,2,3]"
+    StableSet(2, 3, 1).asJson.noSpaces shouldBe "[1,2,3]"
+    StableSet(2, 1, 3).asJson.noSpaces shouldBe "[1,2,3]"
 
   }
 }
