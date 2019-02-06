713a126cbbbd22e9753f4cb8edf205d1f7fb900b
Istvan Bartha <bartha.pityu@gmail.com>
2019-02-06 09:39:56 +0100

    Make old test pass

--- pipeline-executor/src/test/scala/pipelines/PipelinesApplicationTest.scala
+++ pipeline-executor/src/test/scala/pipelines/PipelinesApplicationTest.scala
@@ -73,7 +73,7 @@ class PipelinesApplicationTest
     Then("pastRuns should return two runs")
     result2.size shouldBe 2
     result2.map(_.runId) shouldBe List("", "fake")
-    result2.map(_.runFolderPath) shouldBe List("raw_data//", "fakePath2")
+    result2.map(_.runFolderPath) shouldBe List(Some("raw_data//"), Some("fakePath2"))
   }
 
   test(
