d79cc6c1313439087bfbf15687eeb8e2dbff7c70
Istvan Bartha <bartha.pityu@gmail.com>
2019-03-25 12:56:31 +0100

    Add utility method to split FastQ files
    Uses the intel gkl zlib library

--- pipeline-executor/src/main/scala/pipelines/util/FastQ.scala
+++ pipeline-executor/src/main/scala/pipelines/util/FastQ.scala
@@ -1,8 +1,53 @@
 package org.gc.pipelines.util
 
-import java.io.File
+import java.io.{File, InputStream, FileInputStream}
+import htsjdk.samtools.util._
+import com.intel.gkl.compression.IntelDeflaterFactory
+import com.intel.gkl.compression.IntelInflaterFactory
 
 object FastQHelpers {
+  def splitFastQ(input: File, maxReads: Long): Seq[File] = {
+
+    def openOutputStream[T](fileName: File)(
+        func: java.io.OutputStream => T): T =
+      fileutils.useResource(
+        new BlockCompressedOutputStream(fileName, 0, new IntelDeflaterFactory))(
+        func)
+
+    def openInputStream[T](fileName: File)(func: java.io.InputStream => T): T =
+      fileutils.useResource(
+        new BlockCompressedInputStream(new FileInputStream(fileName),
+                                       true,
+                                       new IntelInflaterFactory))(func)
+
+    def loop(first: Byte,
+             inputStream: InputStream,
+             files: List[File]): List[File] =
+      if (first == -1) files
+      else {
+        val tmp = Files.createTempFile(".gz")
+        var b = first
+        openOutputStream(tmp) { os =>
+          var i = 0L
+          val maxLines = maxReads * 4
+          val lineBreak = '\n'.toByte
+          while (i < maxLines && b >= 0) {
+            if (b == lineBreak) {
+              i += 1L
+            }
+            os.write(b.toInt)
+            b = inputStream.read.toByte
+          }
+        }
+        loop(b, inputStream, tmp :: files)
+
+      }
+
+    openInputStream(input) { inputStream =>
+      loop(inputStream.read.toByte, inputStream, Nil).reverse
+    }
+
+  }
   def getNumberOfReads(file: File): Long = {
 
     fileutils.openSource(file) { source =>
---
+++ pipeline-executor/src/test/scala/pipelines/util/FastQHelpersTest.scala
@@ -0,0 +1,47 @@
+package org.gc.pipelines.util
+
+import org.scalatest._
+import org.scalatest.{Matchers, BeforeAndAfterAll}
+
+import java.io.File
+
+class FastQHelpersTestSuite
+    extends FunSuite
+    with BeforeAndAfterAll
+    with GivenWhenThen
+    with Matchers {
+  test("split fastq.gz") {
+    Given("a fq file with 5k reads")
+    val originalFile =
+      new File(getClass.getResource("/tutorial_8017/papa.read1.bgzip").getFile)
+    When("splitting by 999 reads")
+    val splits = FastQHelpers.splitFastQ(originalFile, 999L)
+    Then("the number of splits should be 10")
+    splits.size shouldBe 6
+    And(" no data should be lost")
+    splits.map(f => FastQHelpers.getNumberOfReads(f)).sum shouldBe FastQHelpers
+      .getNumberOfReads(originalFile)
+    And("concatenating the splits should equal the original")
+    fileutils
+      .openSource(originalFile) { source1 =>
+        splits.foreach { fp =>
+          fileutils.openSource(fp) { source2 =>
+            while (source2.hasNext) {
+              source1.next shouldBe source2.next
+            }
+          }
+        }
+      }
+    And("each partition should be ok to gunzip")
+    splits.foreach { f =>
+      fileutils.openSource(f) { s =>
+        s.mkString
+      }
+    }
+    splits.dropRight(1).foreach { f =>
+      FastQHelpers.getNumberOfReads(f) shouldBe 999
+    }
+    FastQHelpers.getNumberOfReads(splits.last) shouldBe 5
+
+  }
+}
