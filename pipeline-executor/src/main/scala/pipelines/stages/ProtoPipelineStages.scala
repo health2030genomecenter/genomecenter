0adbfdf34ad803b153c129adf57c231772cf6e00
Istvan Bartha <bartha.pityu@gmail.com>
2019-04-10 16:11:42 +0200

    Force rerun all persample-single task
    Reason: some coverage calculations were incorrectly persisted

--- pipeline-executor/src/main/scala/pipelines/stages/ProtoPipelineStages.scala
+++ pipeline-executor/src/main/scala/pipelines/stages/ProtoPipelineStages.scala
@@ -39,7 +39,7 @@ object ProtoPipelineStages extends StrictLogging {
   val singleSampleWES =
     AsyncTask[SingleSamplePipelineInput, SingleSamplePipelineResult](
       "__persample-single",
-      2) {
+      3) {
       case SingleSamplePipelineInput(analysisId,
                                      demultiplexed,
                                      referenceFasta,
